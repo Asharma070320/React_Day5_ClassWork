ad077dc1d01d7967d62c13bcab53c0e3276342c4
AmanAsharma070320 <asharma070320@gmail.com>
2023-12-28 20:24:15 +0530

    Day 5 Classwork

---
+++ src/Component/Main.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+function Main() {
+  let [Subject, setSubject] = useState("");
+  let [Number, setNumber] = useState(0);
+  let [Planner, setPlanner] = useState([]);
+
+  function getSubject(e) {
+    let store1 = e.target.value;
+    // console.log(store1);
+    setSubject(store1);
+  }
+
+  function getNumber(e) {
+    let store2 = e.target.value;
+    // console.log(store2);
+    setNumber(store2);
+  }
+  // console.log(Subject,Number)
+
+  function clickBtn() {
+    if(Subject==""){
+        alert('Please Select the details..')
+    }else{
+        setPlanner([...Planner, { subjectInput: Subject, hourInput: Number }]);
+    }
+  }
+  console.log(Planner);
+
+  return (
+    <div className="center">
+      <div className="mid">
+        <h1 className="light">Geekster Education Planner</h1>
+        <input className="margen" type="text" placeholder="Search" onChange={getSubject} />
+        <input className="margen" type="number" placeholder="Hours" onChange={getNumber} />
+
+        <button className="btn" onClick={clickBtn}>Add+</button>
+
+        {
+            Planner.map((data)=>(
+                <div className="white">
+                {data.subjectInput} - {data.hourInput} Hours
+                </div>
+            ))
+        }
+
+      </div>
+    </div>
+  );
+}
+export default Main;
